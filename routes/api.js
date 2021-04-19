97bff8ce0b1181774327b18602b01fc396e2ba41
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-04-19 12:04:35 -0400

    added route api.workouts and fixed connection to database

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,12 @@
 const router = require("express").Router();
-// const SomeModel = require("../models/SomeModel.js")
+const db = require("../models")
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({}, (err, workoutData) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.json(workoutData);
+    });
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const logger = require("morgan");
 const PORT = process.env.PORT || 3000;
 
+const db = require("./models")
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
 const app = express();
+
+const Workout = require("./models")
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on http://localhost:${PORT} !`);
+  console.log(`App running on http://localhost:${PORT} !`);
 });
