4f8e1cabb35d690959d81042331472e7b4aef73b
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-04-22 17:20:13 -0400

    fixed post route and created update route for workouts

--- routes/api.js
+++ routes/api.js
@@ -13,9 +13,9 @@ router.get("/api/workouts", (req, res) => {
 
 router.get("/api/workouts/range", (req, res) => {
     db.Workout.find()
-        .sort({day: -1})
+        .sort({ day: -1 })
         .limit(7)
-        .sort({day: 1})
+        .sort({ day: 1 })
         .then(workoutData => {
             res.status(200).json(workoutData);
         })
@@ -24,12 +24,22 @@ router.get("/api/workouts/range", (req, res) => {
         })
 })
 
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+        req.params.id,
+        {$push: {exercises: req.body} })
+        .then(workoutData => {
+            res.status(200).json(workoutData)
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        });
+})
+
 router.post("/api/workouts", (req, res) => {
-    const newWorkout = new db.Workout({
-        exercises: req.body
-    });
+    const newWorkout = new db.Workout({});
     newWorkout.save((err) => {
-        if(err) {
+        if (err) {
             return res.status(500).send(err);
         }
         return res.status(200).json(newWorkout);
