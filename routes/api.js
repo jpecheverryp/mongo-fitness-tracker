e1c12db8570fd00476048f12d128efa97f0ce77e
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-04-22 15:05:37 -0400

    created post route

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,23 @@
 const router = require("express").Router();
 const db = require("../models")
+
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({}, (err, workoutData) => {
         if (err) {
-            res.status(500).send(err);
+            return res.status(500).send(err);
         }
-        res.json(workoutData);
+        return res.status(200).json(workoutData);
     });
-})
+});
+
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body, (err, workoutData) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        return res.status(200).json(workoutData);
+    })
+});
+
 
 module.exports = router;
